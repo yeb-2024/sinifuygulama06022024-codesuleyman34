506f6b35d8874101df9693ef74c3d83b5547da12
suleymanenderkacar <kacarsuleymanender@gmail.com>
2025-02-06 18:32:04 +0300

    Anasayfa sınıfı oluşturuldu.

---
+++ SinifUygulama/src/main/java/org/example/Anasayfa.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Anasayfa {
+
+    JFrame frame = new JFrame();
+
+    Anasayfa() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+}
