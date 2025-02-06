e5b50806ce7bbaf75c41f3f1491d7f5a020115f8
suleymanenderkacar <kacarsuleymanender@gmail.com>
2025-02-06 18:38:41 +0300

    Profile butonu eklendi.

--- SinifUygulama/src/main/java/org/example/Anasayfa.java
+++ SinifUygulama/src/main/java/org/example/Anasayfa.java
@@ -5,10 +5,17 @@ import javax.swing.*;
 public class Anasayfa {
 
     JFrame frame = new JFrame();
+    JButton button = new JButton("Profile göster");
 
     Anasayfa() {
+
+        button.setBounds(200, 200, 100, 50);
+        button.setFocusable(false);
+        frame.add(button);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
+        frame.setLayout(null);
         frame.setVisible(true);
     }
 }
